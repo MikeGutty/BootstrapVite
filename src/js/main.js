8e499bea84af0ba3b7bfc346e261204b2fd90d3d
Mike <michael777gutty@gmail.com>
2026-02-07 16:01:06 -0400

    Create project

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+import '../scss/carousel.scss'
+
+// Import all of Bootstrap’s JS
+import * as bootstrap from 'bootstrap'
+import './validation.js'
